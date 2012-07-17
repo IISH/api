de5bb18d96c0a1572c218a6759fe0f18de0a9a78
Lucien van Wouw <lwo@iisg.nl>
2012-07-17 15:20:56 +0200

    Mapping update

--- bootstrap/src/main/java/org/socialhistory/solr/bootstrap/StartUp.java
+++ bootstrap/src/main/java/org/socialhistory/solr/bootstrap/StartUp.java
@@ -8,33 +8,30 @@ import org.mortbay.jetty.webapp.WebAppContext;
  * User: lwo
  * Date: 9-jul-2009
  * Time: 10:49:29
- *
+ * <p/>
  * Startup our Jetty server to bootstrap our projects
  */
 
-public class StartUp
-{
+public class StartUp {
 
     private static StartUp server;
 
-        public static void main(String... args) throws Exception
-        {
-            server = new StartUp();
-            server.Start();
-        }
+    public static void main(String... args) throws Exception {
+        server = new StartUp();
+        server.Start();
+    }
 
-    private Server Start() throws Exception
-    {
+    private Server Start() throws Exception {
         int port = 8080;
         Server server = new Server(port);
 
         // Solr home environment
-        if ( System.getProperty("solr.solr.home") == null )
-            System.setProperty("solr.solr.home", "H:\\projects\\sru-widget\\solr-mappings\\solr");
+        if (System.getProperty("solr.solr.home") == null)
+            System.setProperty("solr.solr.home", "./solr-mappings/solr");
 
         System.setProperty("enable.master", "true");
         System.setProperty("enable.slave", "true");
-        System.setProperty("debug", "true");
+        //System.setProperty("debug", "true");
 
         server.addHandler(new WebAppContext("./solr-mappings/solr-3.4.0.war", "/solr"));
         server.addHandler(new WebAppContext("./solr-mappings/solr/srw", "/styles"));
@@ -42,7 +39,7 @@ public class StartUp
 
         server.start();
 
-        return server ;
+        return server;
     }
 
 
