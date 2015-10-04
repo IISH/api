fe4a3b1e7a7566299522cb6d9e75f1a577dd44d4
Lucien van Wouw <lwo@iisg.nl>
2015-10-04 23:53:49 +0200

    Add the home directory variable to the call.

--- solr/bin/harvest.sh
+++ solr/bin/harvest.sh
@@ -38,7 +38,7 @@ php harvest_oai.php $dataset > /data/log/$dataset.$now.harvest.log
 # Import all records
 # For this we need stylesheets to normalize the marc documents into our model
 app=$API_HOME/solr/lib/import-1.0.jar
-java -cp $app org.socialhistory.solr.importer.BatchImport $f "http://localhost:8080/solr/all/update" "/var/www/api/solr/all/conf/normalize/$dataset.xsl,/data/solr-mappings.index0/solr/all/conf/import/add.xsl,/var/www/api/solr/all/conf/import/addSolrDocument.xsl" "collectionName:$dataset"
+java -cp $app org.socialhistory.solr.importer.BatchImport $f "http://localhost:8080/solr/all/update" "${API_HOME}/solr/all/conf/normalize/${dataset}.xsl,${API_HOME}/solr/all/conf/import/add.xsl,${API_HOME}/solr/all/conf/import/addSolrDocument.xsl" "collectionName:$dataset"
 
 rm $f
 
