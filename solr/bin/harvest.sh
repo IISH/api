646f07ad61628abd9bb3f09b3174d225135bd59d
Lucien van Wouw <lwo@iisg.nl>
2015-10-05 14:05:51 +0200

    Report issues over mail.

--- solr/bin/harvest.sh
+++ solr/bin/harvest.sh
@@ -3,43 +3,93 @@
 # harvest.sh
 # Start an OAI2 harvest
 
+
+
+LOG="/data/log/${dataset}.${now}.harvest.log"
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Make sure we have a home directory.
+#-----------------------------------------------------------------------------------------------------------------------
 if [ -z "$API_HOME" ] ; then
-    echo "Environmental variable API_HOME not set."
+    echo "Environmental variable API_HOME not set." > $LOG
+    cat $LOG
     exit 1
 fi
 
+
+#-----------------------------------------------------------------------------------------------------------------------
+# See if we have a dataset.
+#-----------------------------------------------------------------------------------------------------------------------
 dataset=$1
 if [ "$dataset" == "" ];
 then
-    echo "A dataset is needed to harvest.";
+    echo "A dataset is needed to harvest." > $LOG
+    cat $LOG
     exit 1
 fi
 
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Enter the bin folder and create a link to the data folder for this dataset.
+#-----------------------------------------------------------------------------------------------------------------------
 cd $API_HOME/solr/bin
 d=/data/datasets/$dataset
 mkdir -p $d
 ln -s $d
 
-# Write the desired harvest from parameter
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Write the desired -from harvest parameter.
+#-----------------------------------------------------------------------------------------------------------------------
 now=$(date +"%Y-%m-%d")
 php LastHarvestFile.php "$now" "-10 day" "$d"/last_harvest.txt
 
-f=$d/catalog.xml
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Do not continue if a process is still running for this dataset.
+#-----------------------------------------------------------------------------------------------------------------------
+catalog_file=$d/catalog.xml
 # Any process older than 3 days we assume has a problem. We remove the file.
-find $f -mtime 3 -exec rm {} +
-if [ -f $f ];
+find $catalog_file -mtime 3 -exec rm {} +
+if [ -f $catalog_file ];
 then
-	echo "Already processing... ${f}"
+    subject="Warning while indexing: ${catalog_file}"
+	echo "Already processing... ${catalog_file}">$LOG
+    /usr/bin/sendmail --body "$LOG" --from "$MAIL_FROM" --to "$MAIL_TO" --subject "$subject" --mail_relay "$MAIL_HOST"
 	exit 1
 fi
 
-php harvest_oai.php $dataset > /data/log/$dataset.$now.harvest.log
 
-# Import all records
-# For this we need stylesheets to normalize the marc documents into our model
+#-----------------------------------------------------------------------------------------------------------------------
+# Start the harvest.
+#-----------------------------------------------------------------------------------------------------------------------
+php harvest_oai.php $dataset > $LOG
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Import all records with the import utility.
+#-----------------------------------------------------------------------------------------------------------------------
 app=$API_HOME/solr/lib/import-1.0.jar
-java -cp $app org.socialhistory.solr.importer.BatchImport $f "http://localhost:8080/solr/all/update" "${API_HOME}/solr/all/conf/normalize/${dataset}.xsl,${API_HOME}/solr/all/conf/import/add.xsl,${API_HOME}/solr/all/conf/import/addSolrDocument.xsl" "collectionName:$dataset"
+java -cp $app org.socialhistory.solr.importer.BatchImport $catalog_file "http://localhost:8080/solr/all/update" "${API_HOME}/solr/all/conf/normalize/${dataset}.xsl,${API_HOME}/solr/all/conf/import/add.xsl,${API_HOME}/solr/all/conf/import/addSolrDocument.xsl" "collectionName:$dataset"
+if [[ $? != 0 ]] ; then
+    subject="Error while indexing: ${catalog_file}"
+    echo $subject >> $LOG
+    /usr/bin/sendmail --body "$LOG" --from "$MAIL_FROM" --to "$MAIL_TO" --subject "$subject" --mail_relay "$MAIL_HOST"
+    exit 1
+fi
 
-rm $f
 
+#-----------------------------------------------------------------------------------------------------------------------
+# Cleanup the harvested catalog.
+#-----------------------------------------------------------------------------------------------------------------------
+rm $catalog_file
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Commit. Really not neccessary. But we like to be sure all is in the index and not the transaction log.
+#-----------------------------------------------------------------------------------------------------------------------
 wget -O /dev/null "http://localhost:8080/solr/all/update?commit=true"
+
+
+exit 0
